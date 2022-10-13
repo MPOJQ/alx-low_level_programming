0644e4c67bc518abe6efd5c6d3b33db97945495f
MPOJQ <damstar4life@gmail.com>
2022-10-13 10:18:46 -0700

    alx C programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,10 @@
 #include <unistd.h>
 /**
  * main - Entry point
- *
- * Return: Always 1 (Success)
+ * Return: 1
  */
 int main(void)
 {
-<<<<<<< HEAD
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-=======
-	write(2,"and that piece of art is useful\" -Dora Korper, 2015-10-19\n", 59);
->>>>>>> 5a4ea96da9f2d6ab1c0bf39dbb15cf26ca9a2699
+write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }

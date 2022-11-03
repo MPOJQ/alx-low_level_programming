7bd3e6b4adab260c96446087ad10488e2032aa3d
MPOJQ <damstar4life@gmail.com>
2022-11-03 11:27:56 -0700

    Recursions

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,23 +1,20 @@
-#include "main.h"
+sh: 1: q: not found
+
 /**
- * _puts_recursion - prints a string followed by a newline
- * @s: the string we print
- * Return: void
- */
+ * _puts_recursion - a function that prints a new line
+ *
+ * @s: the string
+ *
+ * Return: nothing
+*/
+
 void _puts_recursion(char *s)
-
 {
-    if (*s)
-    {
-
-        _putchar(*s);
-
-        _puts_recursion(s + 1);
-
-    }
-
-    if (!*s)
-
-    _putchar('\n');
-
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
 }
